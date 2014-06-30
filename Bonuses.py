b928ff1807ad26ed3f54df73ea6275e774c65323
Chris Fuller <chris@cjwfuller.com>
2014-06-30 08:45:28 +0100

    make variable names more appropriate, add comments

--- Bonuses.py
+++ Bonuses.py
@@ -15,12 +15,14 @@ class Bonuses:
 		count = 1
 		awarded = []
 		while count <= extra:
-			maximum = max(y for x, y in enumerate(points) if x not in awarded)
-			maximums = [i for i, j in enumerate(points) if j == maximum]
-			for m in maximums:
+			highest = max(j for i, j in enumerate(points) if i not in awarded)
+			highest_posns = [i for i, j in enumerate(points) if j == highest]
+			for m in highest_posns:
 				bonuses[m] += 1
+				# ensure people only awarded once for highest, second highest, ...
 				awarded.append(m)
 				count += 1
+				# ensure people only awarded once when there are multiple highest points the same
 				if(count > extra):
 					break
 		return bonuses
